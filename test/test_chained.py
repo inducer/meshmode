07f75a8ec8cbb49321731723d7558ba08a41fbfa
Alex Fikl <alexfikl@gmail.com>
2018-05-21 21:03:13 -0500

    connection: add method to collapse chained connection to a direct connection

--- test/test_chained.py
+++ test/test_chained.py
@@ -24,7 +24,6 @@ THE SOFTWARE.
 import os
 
 import numpy as np
-import numpy.linalg as la
 import matplotlib.pyplot as pt
 
 import pyopencl as cl
@@ -39,10 +38,11 @@ from pyopencl.tools import (  # noqa
 import logging
 logger = logging.getLogger(__name__)
 
+
 def create_chained_connection(queue, ndim,
                               nelements=42,
-                              mesh_order=2,
-                              discr_order=2,
+                              mesh_order=5,
+                              discr_order=5,
                               visualize=False):
     ctx = queue.context
     connections = [None, None]
@@ -86,7 +86,7 @@ def create_chained_connection(queue, ndim,
         pt.plot(from_nodes[0], from_nodes[1], 'o', mfc='none')
         pt.plot(to_nodes[0], to_nodes[1], '.')
         filename = os.path.join(os.path.dirname(__file__),
-            "test_chained_0_{}d.png".format(ndim))
+            "test_chained_nodes_0.png".format(ndim))
         pt.tight_layout()
         pt.savefig(filename, dpi=300)
         pt.clf()
@@ -106,7 +106,20 @@ def create_chained_connection(queue, ndim,
         pt.plot(from_nodes[0], from_nodes[1], 'o', mfc='none')
         pt.plot(to_nodes[0], to_nodes[1], '.')
         filename = os.path.join(os.path.dirname(__file__),
-            "test_chained_1_{}d.png".format(ndim))
+            "test_chained_nodes_1.png".format(ndim))
+        pt.tight_layout()
+        pt.savefig(filename, dpi=300)
+        pt.clf()
+
+    if visualize and ndim == 2:
+        from_nodes = connections[0].from_discr.nodes().get(queue)
+        to_nodes = connections[1].to_discr.nodes().get(queue)
+
+        pt.plot(to_nodes[0], to_nodes[1], 'k')
+        pt.plot(from_nodes[0], from_nodes[1], 'o', mfc='none')
+        pt.plot(to_nodes[0], to_nodes[1], '.')
+        filename = os.path.join(os.path.dirname(__file__),
+            "test_chained_nodes_2.png".format(ndim))
         pt.tight_layout()
         pt.savefig(filename, dpi=300)
         pt.clf()
@@ -118,23 +131,147 @@ def create_chained_connection(queue, ndim,
 
 
 @pytest.mark.parametrize("ndim", [2, 3])
-def test_chained_full_resample_matrix(ctx_factory, ndim, visualize=False):
+def test_chained_batch_map(ctx_factory, ndim, visualize=False):
+    ctx = ctx_factory()
+    queue = cl.CommandQueue(ctx)
+
+    chained = create_chained_connection(queue, ndim, visualize=visualize)
+    conn = chained.connections[1]
+    el_to_batch = chained._element_to_batch(queue, conn)
+
+    for igrp, grp in enumerate(conn.groups):
+        for ibatch, batch in enumerate(grp.batches):
+            for p, k in enumerate(batch.from_element_indices.get(queue)):
+                assert (igrp, ibatch, p) in el_to_batch[k]
+
+    for i, batches in enumerate(el_to_batch):
+        for igrp, ibatch, p in batches:
+            batch = conn.groups[igrp].batches[ibatch]
+            assert i == batch.from_element_indices[p]
+
+
+@pytest.mark.parametrize("ndim", [2, 3])
+def test_chained_merge_groups(ctx_factory, ndim, visualize=False):
     ctx = ctx_factory()
     queue = cl.CommandQueue(ctx)
 
     chained = create_chained_connection(queue, ndim, visualize=visualize)
     connections = chained.connections
+    grp_to_grp, grp_to_batch = chained._merge_groups(chained.from_discr,
+                                                     connections[0].groups,
+                                                     connections[1])
 
-    x = connections[0].from_discr.nodes().with_queue(queue)
+    if visualize:
+        import matplotlib.pyplot as pt
+
+        pt.figure(figsize=(10, 8))
+        for k, v in grp_to_grp.items():
+            igrp, ibatch, jgrp, jbatch = k
+            mgroup, mbatch = v
+            from_group_index = connections[0].groups[igrp] \
+                    .batches[ibatch].from_group_index
+
+            from_unit_nodes = connections[0].from_discr \
+                    .groups[from_group_index].unit_nodes
+            to_unit_nodes = grp_to_batch[mgroup][mbatch].result_unit_nodes
+
+            print(k)
+            print(v)
+
+            if ndim == 2:
+                pt.plot(from_unit_nodes, 'o')
+                pt.plot(to_unit_nodes, '^')
+            else:
+                pt.plot(from_unit_nodes[0], from_unit_nodes[1], 'o')
+                pt.plot(to_unit_nodes[0], to_unit_nodes[1], '^')
+
+            pt.savefig('test_grp_to_grp_{}d_{:05d}_{:05d}.png'
+                        .format(ndim, mgroup, mbatch), dpi=300)
+            pt.clf()
+
+
+@pytest.mark.parametrize("ndim", [2, 3])
+def test_chained_full_resample_matrix(ctx_factory, ndim, visualize=False):
+    ctx = ctx_factory()
+    queue = cl.CommandQueue(ctx)
+
+    chained = create_chained_connection(queue, ndim,
+                                        mesh_order=2, discr_order=2,
+                                        visualize=visualize)
+    connections = chained.connections
+
     def f(x):
         from six.moves import reduce
         return 0.1 * reduce(lambda x, y: x * cl.clmath.sin(5 * y), x)
 
-    fx = f(x)
     resample_mat = chained.full_resample_matrix(queue).get(queue)
+
+    x = connections[0].from_discr.nodes().with_queue(queue)
+    fx = f(x)
     f1 = np.dot(resample_mat, fx.get(queue))
     f2 = chained(queue, fx).get(queue)
     f3 = connections[1](queue, connections[0](queue, fx)).get(queue)
+
+    assert np.allclose(f1, f2)
+    assert np.allclose(f2, f3)
+
+
+@pytest.mark.parametrize("ndim", [2, 3])
+def test_chained_to_direct(ctx_factory, ndim, visualize=False):
+    import time
+
+    ctx = ctx_factory()
+    queue = cl.CommandQueue(ctx)
+
+    chained = create_chained_connection(queue, ndim, visualize=visualize)
+    connections = chained.connections
+    direct = chained.direct_connection(queue)
+
+    to_element_indices = np.full(direct.to_discr.mesh.nelements, 0,
+                                 dtype=np.int)
+    for grp in direct.groups:
+        for batch in grp.batches:
+            for i in batch.to_element_indices.get(queue):
+                to_element_indices[i] += 1
+    assert np.min(to_element_indices) > 0
+
+    def f(x):
+        from six.moves import reduce
+        return 0.1 * reduce(lambda x, y: x * cl.clmath.sin(5 * y), x)
+
+    x = connections[0].from_discr.nodes().with_queue(queue)
+    fx = f(x)
+
+    t_start = time.time()
+    f1 = direct(queue, fx).get(queue)
+    t_end = time.time()
+    if visualize:
+        print('[TIME] Direct: {:.5e}'.format(t_end - t_start))
+
+    t_start = time.time()
+    f2 = chained(queue, fx).get(queue)
+    t_end = time.time()
+    if visualize:
+        print('[TIME] Chained: {:.5e}'.format(t_end - t_start))
+
+    t_start = time.time()
+    f3 = connections[1](queue, connections[0](queue, fx)).get(queue)
+    t_end = time.time()
+    if visualize:
+        print('[TIME] Chained: {:.5e}'.format(t_end - t_start))
+
+    if visualize and ndim == 2:
+        import matplotlib.pyplot as pt
+
+        pt.figure(figsize=(10, 8))
+        pt.plot(f1, label='Direct')
+        pt.plot(f2, label='Chained')
+        pt.ylim([np.min(f2) - 0.1, np.max(f2) + 0.1])
+        pt.legend()
+        filename = os.path.join(os.path.dirname(__file__),
+            "test_chained_to_direct_fn.png".format(ndim))
+        pt.savefig(filename, dpi=300)
+        pt.clf()
 
     assert np.allclose(f1, f2)
     assert np.allclose(f2, f3)
