0a025178870c35fe9301b96c08ee16a30c35ec66
Alexandru Fikl <alexfikl@gmail.com>
2020-05-08 19:51:59 -0500

    fix some remaining issues with 1d meshes

--- meshmode/discretization/poly_element.py
+++ meshmode/discretization/poly_element.py
@@ -177,7 +177,9 @@ class QuadratureSimplexElementGroup(SimplexElementGroupBase):
     @memoize_method
     def _quadrature_rule(self):
         dims = self.mesh_el_group.dim
-        if dims == 1:
+        if dims == 0:
+            return mp.Quadrature(np.empty((0, 1)), np.empty((0, 1)))
+        elif dims == 1:
             return mp.LegendreGaussQuadrature(self.order)
         else:
             return mp.XiaoGimbutasSimplexQuadrature(self.order, dims)
