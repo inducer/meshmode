7662f411fa458cac6baceb9e6f437a93ddb2ab36
Andreas Klöckner <inform@tiker.net>
2020-10-15 23:54:41 -0500

    Bump pytools dependency for deprecate_kwarg

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ def main():
               "numpy",
               "modepy",
               "gmsh_interop",
-              "pytools>=2020.3",
+              "pytools>=2020.4.1",
               "pytest>=2.3",
               "loo.py>=2014.1",
               "recursivenodes",
