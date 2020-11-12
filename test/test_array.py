eb4134a07a5473703bc804ab9a06719c3d8fe318
Andreas Kloeckner <inform@tiker.net>
2020-11-11 19:56:40 -0600

    Add test_dof_array_arithmetic_same_as_numpy

--- test/test_array.py
+++ test/test_array.py
@@ -28,9 +28,7 @@ from meshmode.array_context import (  # noqa
         as pytest_generate_tests)
 
 from meshmode.discretization import Discretization
-from meshmode.discretization.poly_element import (
-        PolynomialWarpAndBlendGroupFactory,
-        )
+from meshmode.discretization.poly_element import PolynomialWarpAndBlendGroupFactory
 from meshmode.dof_array import flatten, unflatten
 
 import logging
@@ -66,6 +64,74 @@ def test_array_context_np_workalike(actx_factory):
         assert np.allclose(actx_result, ref_result)
 
 
+def test_dof_array_arithmetic_same_as_numpy(actx_factory):
+    actx = actx_factory()
+
+    from meshmode.mesh.generation import generate_regular_rect_mesh
+    mesh = generate_regular_rect_mesh(
+            a=(-0.5,)*2, b=(0.5,)*2, n=(3,)*2, order=1)
+
+    discr = Discretization(actx, mesh, PolynomialWarpAndBlendGroupFactory(3))
+
+    import operator
+    from pytools import generate_nonnegative_integer_tuples_below as gnitb
+    from random import uniform, randrange
+    for op_func, n_args, use_integers in [
+            (operator.add, 2, False),
+            (operator.sub, 2, False),
+            (operator.mul, 2, False),
+            (operator.truediv, 2, False),
+            (operator.pow, 2, False),
+            # FIXME pyopencl.Array doesn't do mod.
+            #(operator.mod, 2, True),
+            #(operator.mod, 2, False),
+            # FIXME: Two outputs
+            #(divmod, 2, False),
+
+            (operator.and_, 2, True),
+            (operator.xor, 2, True),
+            (operator.or_, 2, True),
+
+            (operator.le, 2, False),
+            (operator.ge, 2, False),
+            (operator.lt, 2, False),
+            (operator.gt, 2, False),
+            (operator.eq, 2, True),
+            (operator.ne, 2, True),
+
+            (operator.pos, 1, False),
+            (operator.neg, 1, False),
+            (operator.abs, 1, False),
+
+            (lambda ary: ary.real, 1, False),
+            (lambda ary: ary.imag, 1, False),
+            ]:
+        for is_array_flags in gnitb(2, n_args):
+            if sum(is_array_flags) == 0:
+                # all scalars, no need to test
+                continue
+            args = [
+                    (0.5+np.random.rand(discr.ndofs)
+                        if not use_integers else
+                        np.random.randint(3, 200, discr.ndofs))
+
+                    if is_array_flag else
+                    (uniform(0.5, 2)
+                        if not use_integers
+                        else randrange(3, 200))
+                    for is_array_flag in is_array_flags]
+            ref_result = op_func(*args)
+
+            actx_args = [
+                    unflatten(actx, discr, actx.from_numpy(arg))
+                    if isinstance(arg, np.ndarray) else arg
+                    for arg in args]
+
+            actx_result = actx.to_numpy(flatten(op_func(*actx_args)))
+
+            assert np.allclose(actx_result, ref_result)
+
+
 def test_dof_array_comparison(actx_factory):
     actx = actx_factory()
 
@@ -73,10 +139,7 @@ def test_dof_array_comparison(actx_factory):
     mesh = generate_regular_rect_mesh(
             a=(-0.5,)*2, b=(0.5,)*2, n=(8,)*2, order=3)
 
-    from meshmode.discretization import Discretization
-    from meshmode.discretization.poly_element import \
-            PolynomialWarpAndBlendGroupFactory as GroupFactory
-    discr = Discretization(actx, mesh, GroupFactory(3))
+    discr = Discretization(actx, mesh, PolynomialWarpAndBlendGroupFactory(3))
 
     import operator
     for op in [
