5d8a14cc8794e1660bf8b2ab8ad25b4f12781932
Nicholas Christensen <njchris2@illinois.edu>
2020-12-01 01:18:12 -0600

    back down pytools and loopy versions for firedrake

--- setup.py
+++ setup.py
@@ -44,12 +44,14 @@ def main():
               "numpy",
               "modepy>=2020.2",
               "gmsh_interop",
-              "pytools>=2020.4.4",
+              "pytools>=2020.4.1",
+              #"pytools>=2020.4.4",
               "pytest>=2.3",
 
               # 2019.1 is required for the Firedrake CIs, which use an very specific
               # version of Loopy.
-              "loopy>=2020.2.2",
+              loopy>=2019.1
+              #"loopy>=2020.2.2",
 
               "recursivenodes",
               "dataclasses; python_version<='3.6'",
