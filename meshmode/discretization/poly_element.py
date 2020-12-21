e401f6132360e3a7a0a80e4986d39bc81546480e
Alexandru Fikl <alexfikl@gmail.com>
2020-12-21 14:16:11 -0600

    add comment for tensor product is_orthogonal check

--- meshmode/discretization/poly_element.py
+++ meshmode/discretization/poly_element.py
@@ -393,6 +393,8 @@ class TensorProductElementGroupBase(PolynomialElementGroupBase,
 
     def is_orthogonal_basis(self):
         try:
+            # NOTE: meshmode kind of assumes that the basis is also orthonormal,
+            # which is why this check is stricter than expected
             return self._basis.orthonormality_weight() == 1
         except mp.BasisNotOrthonormal:
             return False
