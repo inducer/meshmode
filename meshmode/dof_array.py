eb6afa4924d6aefd48d2202578e6b1b3af5fb6fd
Andreas Klöckner <inform@tiker.net>
2021-01-14 22:13:09 -0600

    Re-wrap note about in-place ops for line length

--- meshmode/dof_array.py
+++ meshmode/dof_array.py
@@ -108,9 +108,10 @@ class DOFArray:
         :class:`DOFArray` instances support elementwise ``<``, ``>``,
         ``<=``, ``>=``. (:mod:`numpy` object arrays containing arrays do not.)
 
-    Basic in-place operations are also supported. Note that not all array types provided
-    by :class:`meshmode.array_context.ArrayContext` implementations support
-    in-place operations. Those based on lazy evaluation are a salient example.
+    Basic in-place operations are also supported. Note that not all array types
+    provided by :class:`meshmode.array_context.ArrayContext` implementations
+    support in-place operations. Those based on lazy evaluation are a salient
+    example.
 
     .. automethod:: __iadd__
     .. automethod:: __isub__
