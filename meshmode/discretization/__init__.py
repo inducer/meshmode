6e51d26d9874f1537be04a07568e6b8e59d233c9
Alexandru Fikl <alexfikl@gmail.com>
2021-03-09 21:46:58 -0600

    skip discr node resampling if unit nodes are the same as the mesh

--- meshmode/discretization/__init__.py
+++ meshmode/discretization/__init__.py
@@ -366,16 +366,26 @@ class Discretization:
                     """,
                 name="nodes")
 
+        def resample_mesh_nodes(grp, iaxis, tol=1.0e-15):
+            # TODO: would be nice to have the mesh use an array context already
+            nodes = actx.from_numpy(grp.mesh_el_group.nodes[iaxis])
+
+            grp_unit_nodes = grp.unit_nodes
+            meg_unit_nodes = grp.mesh_el_group.unit_nodes
+            if (grp_unit_nodes.shape == meg_unit_nodes.shape
+                    and np.linalg.norm(grp_unit_nodes - meg_unit_nodes) < tol):
+                return nodes
+
+            return actx.call_loopy(
+                    prg(),
+                    resampling_mat=actx.from_numpy(grp.from_mesh_interp_matrix()),
+                    nodes=nodes,
+                    )["result"]
+
         return make_obj_array([
-            _DOFArray(None, tuple(
-                actx.freeze(
-                    actx.call_loopy(
-                        prg(),
-                        resampling_mat=actx.from_numpy(
-                            grp.from_mesh_interp_matrix()),
-                        nodes=actx.from_numpy(grp.mesh_el_group.nodes[iaxis])
-                        )["result"])
-                for grp in self.groups))
+            _DOFArray(None, tuple([
+                actx.freeze(resample_mesh_nodes(grp, iaxis)) for grp in self.groups
+                ]))
             for iaxis in range(self.ambient_dim)])
 
 # vim: fdm=marker
