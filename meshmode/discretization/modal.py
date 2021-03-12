469c4859799d2528b1cb3196a691313baf6470bc
Thomas Gibson <gibsonthomas1120@hotmail.com>
2021-03-12 10:20:03 -0600

    Copyright edits and remove duplicated code

--- meshmode/discretization/modal.py
+++ meshmode/discretization/modal.py
@@ -1,6 +1,5 @@
 __copyright__ = """
-Copyright (C) 2013-2021 Andreas Kloeckner
-Copyright (C) 2021 Thomas Gibson
+Copyright (C) 2021 University of Illinois Board of Trustees
 """
 
 __license__ = """
@@ -24,49 +23,10 @@ THE SOFTWARE.
 """
 
 
-from pytools import memoize_in, memoize_method
-from pytools.obj_array import make_obj_array
-from meshmode.array_context import make_loopy_program
-
-# underscored because it shouldn't be imported from here.
-from meshmode.dof_array import DOFArray as _DOFArray
-
 from meshmode.discretization import DiscretizationBase
 
 
 class ModalDiscretization(DiscretizationBase):
     """An unstructured composite modal discretization."""
 
-    @memoize_method
-    def modes(self):
-        r"""
-        :returns: object array of shape ``(ambient_dim,)`` containing
-            :class:`~meshmode.dof_array.DOFArray`\ s of modal coefficients.
-        """
-
-        actx = self._setup_actx
-
-        @memoize_in(actx, (ModalDiscretization, "modes_prg"))
-        def prg():
-            return make_loopy_program(
-                """{[iel,idof,j]:
-                    0<=iel<nelements and
-                    0<=idof<ndiscr_nodes and
-                    0<=j<nmesh_nodes}""",
-                """
-                    result[iel, idof] = \
-                        sum(j, resampling_mat[idof, j] * modes[iel, j])
-                    """,
-                name="modes")
-
-        return make_obj_array([
-            _DOFArray(None, tuple(
-                actx.freeze(
-                    actx.call_loopy(
-                        prg(),
-                        resampling_mat=actx.from_numpy(
-                            grp.from_mesh_interp_matrix()),
-                        nodes=actx.from_numpy(grp.mesh_el_group.nodes[iaxis])
-                        )["result"])
-                for grp in self.groups))
-            for iaxis in range(self.ambient_dim)])
+    # Class methods TBD
