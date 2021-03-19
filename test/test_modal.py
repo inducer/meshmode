4e0520715ec763ae453fd058a9a725fdb838cbcb
Thomas Gibson <gibsonthomas1120@hotmail.com>
2021-03-19 15:33:27 -0500

    Test modal coefficients and mappings for quadrature-based groups

--- test/test_modal.py
+++ test/test_modal.py
@@ -21,19 +21,15 @@ THE SOFTWARE.
 """
 
 
-from dataclasses import dataclass
 from functools import partial
 import numpy as np
-import numpy.linalg as la
-
-from pytools.obj_array import make_obj_array
 
 import meshmode                       # noqa: F401
 from meshmode.array_context import (  # noqa
     pytest_generate_tests_for_pyopencl_array_context
     as pytest_generate_tests
     )
-
+from meshmode.dof_array import DOFArray
 from meshmode.mesh import (
     SimplexElementGroup,
     TensorProductElementGroup
@@ -60,10 +56,9 @@ from meshmode.discretization.connection.modal import (
     ModalInverseDiscretizationConnection
     )
 
-from meshmode.dof_array import thaw, flatten
+from meshmode.dof_array import thaw
 
 import meshmode.mesh.generation as mgen
-
 import pytest
 
 
@@ -99,9 +94,11 @@ def test_inverse_modal_connections(actx_factory, nodal_group_factory):
 
     # Make connections
     nodal_to_modal_conn = ModalDiscretizationConnection(
-        nodal_disc, modal_disc)
+        nodal_disc, modal_disc
+    )
     modal_to_nodal_conn = ModalInverseDiscretizationConnection(
-        modal_disc, nodal_disc)
+        modal_disc, nodal_disc
+    )
 
     x_nodal = thaw(actx, nodal_disc.nodes()[0])
     nodal_f = f(x_nodal)
@@ -114,20 +111,19 @@ def test_inverse_modal_connections(actx_factory, nodal_group_factory):
     # This error should be small since we composed a map with
     # its inverse
     err = actx.np.linalg.norm(nodal_f - nodal_f_2)
-    assert err <= 1e-12
+
+    assert err <= 1e-13
 
 
 @pytest.mark.parametrize("quad_group_factory", [
-    InterpolatoryQuadratureSimplexGroupFactory
-    #QuadratureSimplexGroupFactory
+    QuadratureSimplexGroupFactory
     ])
-def test_quadrature_based_modal_connection(actx_factory, quad_group_factory):
-
+def test_modal_coefficients_by_projection(actx_factory, quad_group_factory):
     group_cls = SimplexElementGroup
     modal_group_factory = ModalSimplexGroupFactory
     actx = actx_factory()
-    order = 4
-    m_order = 4
+    order = 10
+    m_order = 5
 
     # Make a regular rectangle mesh
     mesh = mgen.generate_regular_rect_mesh(
@@ -137,27 +133,91 @@ def test_quadrature_based_modal_connection(actx_factory, quad_group_factory):
     nodal_disc = NodalDiscretization(actx, mesh, quad_group_factory(order))
     modal_disc = ModalDiscretization(actx, mesh, modal_group_factory(m_order))
 
-    # Make connections one using quadrature projection and one using
-    # the Vandermonde inverse
+    # Make connections one using quadrature projection
     nodal_to_modal_conn_quad = ModalDiscretizationConnection(
-        nodal_disc, modal_disc, allow_approximate_quad=True)
-    nodal_to_modal_conn_vinv = ModalDiscretizationConnection(
-        nodal_disc, modal_disc, allow_approximate_quad=False)
+        nodal_disc, modal_disc, allow_approximate_quad=True
+    )
 
     def f(x):
-        return 0.25*actx.np.cos(2*x)
+        return 2*actx.np.sin(5*x)
+
+    x_nodal = thaw(actx, nodal_disc.nodes()[0])
+    nodal_f = f(x_nodal)
+
+    # Compute modal coefficients we expect to get
+    import modepy as mp
+
+    grp, = nodal_disc.groups
+    shape = mp.Simplex(grp.dim)
+    space = mp.space_for_shape(shape, order=m_order)
+    basis = mp.orthonormal_basis_for_space(space, shape)
+    quad = grp._quadrature_rule()
+
+    nodal_f_data = actx.to_numpy(nodal_f[0])
+    vdm = mp.vandermonde(basis.functions, quad.nodes)
+    w_diag = np.diag(quad.weights)
+
+    modal_data = []
+    for _, nodal_data in enumerate(nodal_f_data):
+        # Compute modal data in each element: V.T * W * nodal_data
+        elem_modal_f = np.dot(vdm.T, np.dot(w_diag, nodal_data))
+        modal_data.append(elem_modal_f)
+
+    modal_data = actx.from_numpy(np.asarray(modal_data))
+    modal_f_expected = DOFArray(actx, data=(modal_data,))
+
+    # Map nodal coefficients using the quadrature-based projection
+    modal_f_computed = nodal_to_modal_conn_quad(nodal_f)
+
+    err = actx.np.linalg.norm(modal_f_expected - modal_f_computed)
+
+    assert err <= 1e-13
+
+
+@pytest.mark.parametrize("quad_group_factory", [
+    QuadratureSimplexGroupFactory
+    ])
+def test_quadrature_based_modal_connection_reverse(actx_factory, quad_group_factory):
+
+    group_cls = SimplexElementGroup
+    modal_group_factory = ModalSimplexGroupFactory
+    actx = actx_factory()
+    order = 10
+    m_order = 5
+
+    # Make a regular rectangle mesh
+    mesh = mgen.generate_regular_rect_mesh(
+        a=(0, 0)*2, b=(5, 3), n=(10, 6,), order=order, group_cls=group_cls)
+
+    # Make discretizations
+    nodal_disc = NodalDiscretization(actx, mesh, quad_group_factory(order))
+    modal_disc = ModalDiscretization(actx, mesh, modal_group_factory(m_order))
+
+    # Make connections one using quadrature projection
+    nodal_to_modal_conn_quad = ModalDiscretizationConnection(
+        nodal_disc, modal_disc
+    )
+
+    # And the reverse connection
+    modal_to_nodal_conn = ModalInverseDiscretizationConnection(
+        modal_disc, nodal_disc
+    )
+
+    def f(x):
+        return 1 + 2*x + 3*x**2
 
     x_nodal = thaw(actx, nodal_disc.nodes()[0])
     nodal_f = f(x_nodal)
 
     # Map nodal coefficients using the quadrature-based projection
     modal_f_quad = nodal_to_modal_conn_quad(nodal_f)
-    modal_f_vinv = nodal_to_modal_conn_vinv(nodal_f)
 
+    # Back to nodal
+    nodal_f_computed = modal_to_nodal_conn(modal_f_quad)
 
-    err = actx.np.linalg.norm(modal_f_quad - modal_f_vinv)
-    assert err <= 1e-8
+    err = actx.np.linalg.norm(nodal_f - nodal_f_computed)
 
+    assert err <= 1e-11
 
 
 if __name__ == "__main__":
