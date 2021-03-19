7c599f38620548e2d1494752c265791a5df7b0ec
Thomas Gibson <gibsonthomas1120@hotmail.com>
2021-03-19 10:38:14 -0500

    Update NodalDiscretization `nodes` method

--- meshmode/discretization/nodal.py
+++ meshmode/discretization/nodal.py
@@ -74,40 +74,6 @@ class NodalDiscretization(DiscretizationBase):
     def ndofs(self):
         return sum(grp.ndofs for grp in self.groups)
 
-    @memoize_method
-    def nodes(self):
-        r"""
-        :returns: object array of shape ``(ambient_dim,)`` containing
-            :class:`~meshmode.dof_array.DOFArray`\ s of node coordinates.
-        """
-
-        actx = self._setup_actx
-
-        @memoize_in(actx, (NodalDiscretization, "nodes_prg"))
-        def prg():
-            return make_loopy_program(
-                """{[iel,idof,j]:
-                    0<=iel<nelements and
-                    0<=idof<ndiscr_nodes and
-                    0<=j<nmesh_nodes}""",
-                """
-                    result[iel, idof] = \
-                        sum(j, resampling_mat[idof, j] * nodes[iel, j])
-                    """,
-                name="nodes")
-
-        return make_obj_array([
-            _DOFArray(None, tuple(
-                actx.freeze(
-                    actx.call_loopy(
-                        prg(),
-                        resampling_mat=actx.from_numpy(
-                            grp.from_mesh_interp_matrix()),
-                        nodes=actx.from_numpy(grp.mesh_el_group.nodes[iaxis])
-                        )["result"])
-                for grp in self.groups))
-            for iaxis in range(self.ambient_dim)])
-
     def num_reference_derivative(self, ref_axes, vec):
         actx = vec.array_context
         ref_axes = list(ref_axes)
@@ -159,6 +125,52 @@ class NodalDiscretization(DiscretizationBase):
                         nelements=grp.nelements,
                         )["result"])
                 for grp in self.groups))
+
+    @memoize_method
+    def nodes(self):
+        r"""
+        :returns: object array of shape ``(ambient_dim,)`` containing
+            :class:`~meshmode.dof_array.DOFArray`\ s of node coordinates.
+        """
+
+        actx = self._setup_actx
+
+        @memoize_in(actx, (NodalDiscretization, "nodes_prg"))
+        def prg():
+            return make_loopy_program(
+                """{[iel,idof,j]:
+                    0<=iel<nelements and
+                    0<=idof<ndiscr_nodes and
+                    0<=j<nmesh_nodes}""",
+                """
+                    result[iel, idof] = \
+                        sum(j, resampling_mat[idof, j] * nodes[iel, j])
+                    """,
+                name="nodes")
+
+        def resample_mesh_nodes(grp, iaxis):
+            # TODO: would be nice to have the mesh use an array context already
+            nodes = actx.from_numpy(grp.mesh_el_group.nodes[iaxis])
+
+            grp_unit_nodes = grp.unit_nodes.reshape(-1)
+            meg_unit_nodes = grp.mesh_el_group.unit_nodes.reshape(-1)
+
+            tol = 10 * np.finfo(grp_unit_nodes.dtype).eps
+            if (grp_unit_nodes.shape == meg_unit_nodes.shape
+                    and np.linalg.norm(grp_unit_nodes - meg_unit_nodes) < tol):
+                return nodes
+
+            return actx.call_loopy(
+                    prg(),
+                    resampling_mat=actx.from_numpy(grp.from_mesh_interp_matrix()),
+                    nodes=nodes,
+                    )["result"]
+
+        return make_obj_array([
+            _DOFArray(None, tuple([
+                actx.freeze(resample_mesh_nodes(grp, iaxis)) for grp in self.groups
+                ]))
+            for iaxis in range(self.ambient_dim)])
 
 
 class Discretization(NodalDiscretization):
