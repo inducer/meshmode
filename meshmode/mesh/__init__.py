226f0797475c82b54ffa7678d46b0afc9931e99c
Andreas Klöckner <inform@tiker.net>
2021-06-03 17:00:12 -0500

    Narrow fagrp error check in mesh-to-python

--- meshmode/mesh/__init__.py
+++ meshmode/mesh/__init__.py
@@ -1273,8 +1273,8 @@ def as_python(mesh, function_name="make_mesh"):
         # {{{ facial adjacency groups
 
         def fagrp_params_str(fagrp):
-            if isinstance(fagrp, InterPartitionAdjacencyGroup):
-                raise NotImplementedError("Not implemented for distributed meshes.")
+            if not isinstance(fagrp, FacialAdjacencyGroup):
+                raise NotImplementedError(f"Not implemented for {fagrp.__class__}.")
             params = {
                     "igroup": fagrp.igroup,
                     "ineighbor_group": repr(fagrp.ineighbor_group),
