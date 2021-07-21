96be62f6004d337a26a3ff565068b13b5314c0d8
Alex Fikl <alexfikl@gmail.com>
2021-07-21 18:14:12 -0500

    clarify message on __bool__ error
    Co-authored-by: Andreas Klöckner <inform@tiker.net>

--- meshmode/dof_array.py
+++ meshmode/dof_array.py
@@ -175,7 +175,7 @@ class DOFArray:
 
     def __bool__(self):
         raise ValueError(
-                "The truth value of a DOFArray is ambiguous. "
+                "The truth value of a DOFArray is not well-defined. "
                 "Use actx.np.any(x) or actx.np.all(x)")
 
     def __len__(self):
