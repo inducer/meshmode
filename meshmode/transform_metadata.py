8e85fefb19d87f82d0add0795a6e4b0bc71c9955
Nicholas Christensen <nicholas.j.christensen@outlook.com>
2022-02-07 18:07:48 -0500

    Update documentation

--- meshmode/transform_metadata.py
+++ meshmode/transform_metadata.py
@@ -90,7 +90,7 @@ class IsOpArray(Tag):
 
 
 class EinsumArgsTags(Tag):
-    """A tag containing a FrozenMap of lists of tags indexed by argument name."""
+    """A tag containing an `immutables.Map` of tuples of tags indexed by argument name."""
 
     def __init__(self, tags_map: Map):
         self.tags_map = tags_map
