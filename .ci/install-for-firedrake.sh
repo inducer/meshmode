f07a64c2fc851c1d726444c2fdf4946ec6ef267c
Andreas Kloeckner <inform@tiker.net>
2023-11-15 11:10:44 -0600

    Firedrake CI: Install pocl via PyPI

--- .ci/install-for-firedrake.sh
+++ .ci/install-for-firedrake.sh
@@ -5,10 +5,6 @@ sudo apt update
 sudo apt upgrade -y
 sudo apt install time
 
-# Otherwise we get missing CL symbols
-export PYOPENCL_CL_PRETEND_VERSION=2.1
-sudo apt install -y pocl-opencl-icd ocl-icd-opencl-dev
-
 . /home/firedrake/firedrake/bin/activate
 grep -v loopy requirements.txt > /tmp/myreq.txt
 
@@ -16,14 +12,14 @@ grep -v loopy requirements.txt > /tmp/myreq.txt
 sed -i "/boxtree/ d" /tmp/myreq.txt
 sed -i "/sumpy/ d" /tmp/myreq.txt
 sed -i "/pytential/ d" /tmp/myreq.txt
+sed -i "/pyopencl/ d" /tmp/myreq.txt
 
 # This shouldn't be necessary, but...
 # https://github.com/inducer/meshmode/pull/48#issuecomment-687519451
 pip install pybind11
-
 pip install pytest
-
 pip install -r /tmp/myreq.txt
+pip install pyopencl[pocl]
 
 # Context: https://github.com/OP2/PyOP2/pull/605
 python -m pip install --force-reinstall git+https://github.com/inducer/pytools.git
