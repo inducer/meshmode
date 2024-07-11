57e4a15769be89355789b35121c47c39eb0eecea
Andreas Kloeckner <inform@tiker.net>
2024-07-11 15:21:20 -0500

    Drop bcast_numpy_array on DOFArray

--- meshmode/dof_array.py
+++ meshmode/dof_array.py
@@ -75,7 +75,6 @@ __doc__ = """
 
 @with_container_arithmetic(
         bcast_obj_array=True,
-        bcast_numpy_array=True,
         rel_comparison=True,
         bitwise=True,
         _cls_has_array_context_attr=True)
