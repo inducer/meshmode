5eb0310ef4c427344e8dfa37536ae779f999c047
Alexandru Fikl <alexfikl@gmail.com>
2024-08-06 10:08:21 +0300

    remove some long deprecated functions

--- meshmode/discretization/connection/direct.py
+++ meshmode/discretization/connection/direct.py
@@ -459,13 +459,6 @@ class DirectDiscretizationConnection(DiscretizationConnection):
                 for i_tgrp, cgrp in enumerate(self.groups)
                 for i_batch in range(len(cgrp.batches)))
 
-    def full_resample_matrix(self, actx: ArrayContext):
-        from warnings import warn
-        warn("This method is deprecated. Use 'make_direct_full_resample_matrix' "
-                "instead.", DeprecationWarning, stacklevel=2)
-
-        return make_direct_full_resample_matrix(actx, self)
-
     # {{{ _global_point_pick_info_cache
 
     def _per_target_group_pick_info(
